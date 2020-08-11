a95544ffe828fee003d9cc7ef6cdeeec515b196e
hermanthegerman2 <doetsch.hermann@t-online.de>
2020-08-11 22:50:34 +0200

    initial commit

--- OWServer Discovery/module.php
+++ OWServer Discovery/module.php
@@ -1,5 +1,37 @@
 <?php
+declare(strict_types=1);
+/*
+ * @addtogroup OWServer
+ * @{
+ *
+ * @package       OWServer
+ * @file          module.php
+ * @author        Hermann Dötsch
+ * @copyright     2020 Hermann Dötsch
+ * @license       https://creativecommons.org/licenses/by-nc-sa/4.0/ CC BY-NC-SA 4.0
+ * @version       1.0
+ *
+ */
+require_once __DIR__ . '/../libs/DebugHelper.php';  // diverse Klassen
+
+/**
+ * OWServerDiscovery Klasse implementiert.
+ *
+ * @author        Hermann Dötsch
+ * @copyright     2020 Hermann Dötsch
+ * @license       https://creativecommons.org/licenses/by-nc-sa/4.0/ CC BY-NC-SA 4.0
+ * @version       1.0
+ *
+ * @example <b>Ohne</b>
+ *
+ * @property array $Devices
+ */
 	class OWServerDiscovery extends IPSModule {
+        use \OWServer\DebugHelper;
+
+        /**
+         * Interne Funktion des SDK.
+         */
 
 		public function Create()
 		{
@@ -7,16 +39,143 @@
 			parent::Create();
 		}
 
-		public function Destroy()
-		{
-			//Never delete this line!
-			parent::Destroy();
-		}
-
 		public function ApplyChanges()
 		{
-			//Never delete this line!
+            $this->RegisterMessage(0, IPS_KERNELSTARTED);
+		    //Never delete this line!
 			parent::ApplyChanges();
 		}
+
+        /**
+         * Interne Funktion des SDK.
+         */
+        public function GetConfigurationForm()
+        {
+            $Devices = $this->DiscoverDevices();
+            $Form = json_decode(file_get_contents(__DIR__ . '/form.json'), true);
+            $IPSDevices = $this->GetIPSInstances();
+
+            $Values = [];
+
+            foreach ($Devices as $IPAddress => $Device) {
+                $AddValue = [
+                    'IPAddress'  => $IPAddress,
+                    'servername' => $Device['ENAME'],
+                    'name'       => 'OWServer (' . $Device['ENAME'] . ')',
+                    'version'    => $Device['VERS'],
+                    'instanceID' => 0
+                ];
+                $InstanceID = array_search($IPAddress, $IPSDevices);
+                if ($InstanceID === false) {
+                    $InstanceID = array_search(strtolower($Device['Host']), $IPSDevices);
+                    if ($InstanceID !== false) {
+                        $AddValue['IPAddress'] = $Device['Host'];
+                    }
+                }
+                if ($InstanceID !== false) {
+                    unset($IPSDevices[$InstanceID]);
+                    $AddValue['name'] = IPS_GetLocation($InstanceID);
+                    $AddValue['instanceID'] = $InstanceID;
+                }
+
+                $AddValue['create'] = [
+                    [
+                        'moduleID'      => '{D07693A2-5469-47B9-5AFD-9658DF201D32}',
+                        'configuration' => new stdClass()
+                    ],
+                    [
+                        'moduleID'      => '{CFDFE5FC-C6AE-F6EB-D449-89A2A1F25794}',
+                        'configuration' => [
+                            'Webport' => (int) $Device['JSON']
+                        ]
+                    ],
+                    [
+                        'moduleID'      => '{3CFF0FD9-E306-41DB-9B5A-9D06D38576C3}',
+                        'configuration' => [
+                            'Host' => $AddValue['IPAddress'],
+                            'Port' => 4304
+                        ]
+                    ]
+                ];
+                $Values[] = $AddValue;
+            }
+
+            foreach ($IPSDevices as $InstanceID => $IPAddress) {
+                $Values[] = [
+                    'IPAddress'  => $IPAddress,
+                    'version'    => '',
+                    'servername' => '',
+                    'name'       => IPS_GetLocation($InstanceID),
+                    'instanceID' => $InstanceID
+                ];
+            }
+            $Form['actions'][1]['values'] = $Values;
+            $this->SendDebug('FORM', json_encode($Form), 0);
+            $this->SendDebug('FORM', json_last_error_msg(), 0);
+            return json_encode($Form);
+        }
+
+        private function GetIPSInstances(): array
+        {
+            $InstanceIDList = IPS_GetInstanceListByModuleID('{D07693A2-5469-47B9-5AFD-9658DF201D32}');
+            $Devices = [];
+            foreach ($InstanceIDList as $InstanceID) {
+                $Splitter = IPS_GetInstance($InstanceID)['ConnectionID'];
+                if ($Splitter > 0) {
+                    $IO = IPS_GetInstance($Splitter)['ConnectionID'];
+                    if ($IO > 0) {
+                        $Devices[$InstanceID] = strtolower(IPS_GetProperty($IO, 'Host'));
+                    }
+                }
+            }
+            $this->SendDebug('IPS Devices', $Devices, 0);
+            return $Devices;
+        }
+
+        private function DiscoverDevices(): array
+        {
+            $this->LogMessage($this->Translate('Background discovery of OWServers'), KL_NOTIFY);
+            $socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+            if (!$socket) {
+                return [];
+            }
+            socket_set_option($socket, SOL_SOCKET, SO_BROADCAST, 1);
+            socket_set_option($socket, SOL_SOCKET, SO_REUSEADDR, 1);
+            socket_set_option($socket, SOL_SOCKET, SO_RCVTIMEO, ['sec' => 1, 'usec' => 100000]);
+            socket_bind($socket, '0.0.0.0', 0);
+            $message = "\x65\x49\x50\x41\x44\x00\x4e\x41\x4d\x45\x00\x4a\x53\x4f\x4e\x00\x56\x45\x52\x53\x00\x55\x55\x49\x44\x00\x4a\x56\x49\x44\x06\x12\x34\x56\x78\x12\x34";
+            $this->SendDebug('Search', $message, 1);
+            if (@socket_sendto($socket, $message, strlen($message), 0, '255.255.255.255', 4304) === false) {
+                return [];
+            }
+            usleep(100000);
+            $i = 50;
+            $buf = '';
+            $IPAddress = '';
+            $Port = 0;
+            $DeviceData = [];
+            while ($i) {
+                $ret = @socket_recvfrom($socket, $buf, 2048, 0, $IPAddress, $Port);
+                if ($ret === false) {
+                    break;
+                }
+                if ($ret === 0) {
+                    $i--;
+                    continue;
+                }
+                $this->SendDebug('Receive', $buf, 0);
+                $Serach = ['UUID', 'ENAME', 'VERS', 'JSON'];
+                foreach ($Serach as $Key) {
+                    $start = strpos($buf, $Key);
+                    if ($start !== false) {
+                        $DeviceData[$IPAddress][$Key] = substr($buf, $start + strlen($Key) + 1, ord($buf[$start + strlen($Key)]));
+                    }
+                }
+                $DeviceData[$IPAddress]['Host'] = gethostbyaddr($IPAddress);
+            }
+            socket_close($socket);
+            $this->SendDebug('Found', $DeviceData, 0);
+            return $DeviceData;
+        }
 
 	}
--- OWServer Konfigurator/module.php
+++ OWServer Konfigurator/module.php
@@ -1,22 +1,398 @@
 <?php
+
+declare(strict_types=1);
+
+/*
+ * @addtogroup OWServer
+ * @{
+ *
+ * @package       OWServer
+ * @file          module.php
+ * @author        Hermann Dötsch
+ * @copyright     2020 Hermann Dötsch
+ * @license       https://creativecommons.org/licenses/by-nc-sa/4.0/ CC BY-NC-SA 4.0
+ * @version       1.0
+ *
+ */
+require_once __DIR__ . '/../libs/DebugHelper.php';  // diverse Klassen
+require_once __DIR__ . '/../libs/SqueezeBoxClass.php';  // diverse Klassen
+eval('declare(strict_types=1);namespace LMSConfigurator {?>' . file_get_contents(__DIR__ . '/../libs/helper/BufferHelper.php') . '}');
+eval('declare(strict_types=1);namespace LMSConfigurator {?>' . file_get_contents(__DIR__ . '/../libs/helper/ParentIOHelper.php') . '}');
+
+/**
+ * OWServerKonfigurator Klasse für ein OWServer Konfigurator.
+ * Erweitert IPSModule.
+ *
+ * @author        Hermann Dötsch
+ * @copyright     2020 Hermann Dötsch
+ * @license       https://creativecommons.org/licenses/by-nc-sa/4.0/ CC BY-NC-SA 4.0
+ * @version       1.0
+ *
+ * @example <b>Ohne</b>
+ */
 	class OWServerKonfigurator extends IPSModule {
+        use \OWServer\DebugHelper,
+            \OWServerKonfigurator\BufferHelper,
+            \OWServerKonfigurator\InstanceStatus {
+            \OWServerKonfigurator\InstanceStatus::MessageSink as IOMessageSink;
+            \OWServerKonfigurator\InstanceStatus::RegisterParent as IORegisterParent;
+            \OWServerKonfigurator\InstanceStatus::RequestAction as IORequestAction;
+        }
+
+        /**
+         * Interne Funktion des SDK.
+         */
 
 		public function Create()
 		{
 			//Never delete this line!
 			parent::Create();
-		}
-
-		public function Destroy()
-		{
-			//Never delete this line!
-			parent::Destroy();
+            $this->ConnectParent('{CFDFE5FC-C6AE-F6EB-D449-89A2A1F25794}');
+            $this->SetReceiveDataFilter('.*"nothingtoreceive":.*');
+            $this->ParentID = 0;
 		}
 
 		public function ApplyChanges()
 		{
-			//Never delete this line!
-			parent::ApplyChanges();
-		}
 
+            $this->ParentID = 0;
+            $this->SetReceiveDataFilter('.*"nothingtoreceive":.*');
+            $this->RegisterMessage(0, IPS_KERNELSTARTED);
+            $this->RegisterMessage($this->InstanceID, FM_CONNECT);
+            $this->RegisterMessage($this->InstanceID, FM_DISCONNECT);
+            //Never delete this line!
+            parent::ApplyChanges();
+            if (IPS_GetKernelRunlevel() != KR_READY) {
+                return;
+            }
+
+            $this->RegisterParent();
+            if ($this->HasActiveParent()) {
+                $this->IOChangeState(IS_ACTIVE);
+            }
+        }
+
+        /**
+         * Interne Funktion des SDK.
+         *
+         *
+         * @param type $TimeStamp
+         * @param type $SenderID
+         * @param type $Message
+         * @param type $Data
+         */
+        public function MessageSink($TimeStamp, $SenderID, $Message, $Data)
+        {
+            $this->IOMessageSink($TimeStamp, $SenderID, $Message, $Data);
+
+            switch ($Message) {
+                case IPS_KERNELSTARTED:
+                    $this->KernelReady();
+                    break;
+            }
+        }
+
+        public function RequestAction($Ident, $Value)
+        {
+            if ($this->IORequestAction($Ident, $Value)) {
+                return true;
+            }
+            return false;
+        }
+
+        /**
+         * Interne Funktion des SDK.
+         */
+        public function GetConfigurationForm()
+        {
+            $Form = json_decode(file_get_contents(__DIR__ . '/form.json'), true);
+
+            if (!$this->HasActiveParent()) {
+                $Form['actions'][] = [
+                    'type'  => 'PopupAlert',
+                    'popup' => [
+                        'items' => [[
+                            'type'    => 'Label',
+                            'caption' => 'Instance has no active parent.'
+                        ]]
+                    ]
+                ];
+                $this->SendDebug('FORM', json_encode($Form), 0);
+                $this->SendDebug('FORM', json_last_error_msg(), 0);
+
+                return json_encode($Form);
+            }
+            $Splitter = IPS_GetInstance($this->InstanceID)['ConnectionID'];
+            $IO = IPS_GetInstance($Splitter)['ConnectionID'];
+            $ParentCreate = [
+                [
+                    'moduleID'      => '{CFDFE5FC-C6AE-F6EB-D449-89A2A1F25794}',
+                    'configuration' => [
+                        'Port'     => IPS_GetProperty($Splitter, 'Port'),
+                    ]
+                ],
+                [
+                    'moduleID'      => '{3CFF0FD9-E306-41DB-9B5A-9D06D38576C3}',
+                    'configuration' => [
+                        'Host' => IPS_GetProperty($IO, 'Host'),
+                        'Port' => (int) IPS_GetProperty($IO, 'Port')
+                    ]
+                ]
+            ];
+
+            $FoundDS18B20 = $FoundDS2413 = $FoundDS2450 = $this->GetDeviceInfo();
+            //$FoundServers = array_filter($FoundDS18B20, [$this, 'FilterDS18B20']);
+            $this->SendDebug('Found DS18B20', $FoundDS18B20, 0);
+            $this->SendDebug('Found DS2413', $FoundDS2413, 0);
+            $this->SendDebug('Found DS2450', $FoundDS2450, 0);
+            $InstanceIDListDS18B20 = $this->GetInstanceList('{2E207819-9E78-61B7-D7DA-E89D749B8A30}', 'Address');
+            $this->SendDebug('OWServer DS18B20', $InstanceIDListDS18B20, 0);
+            $InstanceIDListDS2413 = $this->GetInstanceList('{6C06CC7D-13CD-69CD-F932-96AE76FC6C6C}', 'Address');
+            $this->SendDebug('OWServer DS2413', $InstanceIDListDS2413, 0);
+            $InstanceIDListDS2450 = $this->GetInstanceList('{B2963A2B-C5F8-03C3-CC97-6A81A0225EC0}', 'Address');
+            $this->SendDebug('OWServer DS2450', $InstanceIDListDS2450, 0);
+
+            // DS18B20
+            $DS18B20Values = [];
+            foreach ($FoundDS18B20 as $Address => $Device) {
+                $InstanceID = array_search($Address, $InstanceIDListDS18B20);
+                if ($InstanceID !== false) {
+                    $AddValue = [
+                        'instanceID' => $InstanceID,
+                        'name'       => IPS_GetName($InstanceID),
+                        'model'      => ucfirst($Device['model']),
+                        'address'    => $Address,
+                        'location'   => stristr(IPS_GetLocation($InstanceID), IPS_GetName($InstanceID), true)
+                    ];
+                    unset($InstanceIDListDS18B20[$InstanceID]);
+                } else {
+                    $AddValue = [
+                        'instanceID' => 0,
+                        'name'       => $Device['name'],
+                        'model'      => ucfirst($Device['model']),
+                        'address'    => $Address,
+                        'location'   => ''
+                    ];
+                }
+                $Create = [
+                    'moduleID'      => '{2E207819-9E78-61B7-D7DA-E89D749B8A30}',
+                    'configuration' => ['Address' => $Address]
+                ];
+
+                $AddValue['create'] = array_merge([$Create], $ParentCreate);
+                $DS18B20Values[] = $AddValue;
+            }
+            foreach ($InstanceIDListDS18B20 as $InstanceID => $Address) {
+                $Values[] = [
+                    'instanceID' => $InstanceID,
+                    'name'       => IPS_GetName($InstanceID),
+                    'model'      => 'unknown',
+                    'address'    => $Address,
+                    'location'   => stristr(IPS_GetLocation($InstanceID), IPS_GetName($InstanceID), true)
+                ];
+            }
+
+            // DS2413
+            $DS2413Values = [];
+            foreach ($FoundAlarms as $Address => $Device) {
+                $InstanceID = array_search($Address, $InstanceIDListDS2413);
+                if ($InstanceID !== false) {
+                    $AddValue = [
+                        'instanceID' => $InstanceID,
+                        'name'       => IPS_GetName($InstanceID),
+                        'address'    => $Address,
+                        'location'   => IPS_GetLocation($InstanceID)
+                    ];
+                    unset($InstanceIDListDS2413[$InstanceID]);
+                } else {
+                    $AddValue = [
+                        'instanceID' => 0,
+                        'name'       => $this->Translate('Alarm') . ' ' . $Device['name'],
+                        'address'    => $Address,
+                        'location'   => ''
+                    ];
+                }
+                $Create = [
+                    'moduleID'      => '{6C06CC7D-13CD-69CD-F932-96AE76FC6C6C}',
+                    'configuration' => ['Address' => $Address]
+                ];
+
+                $AddValue['create'] = array_merge([$Create], $ParentCreate);
+                $DS2413Values[] = $AddValue;
+            }
+            foreach ($InstanceIDListDS2413 as $InstanceID => $Address) {
+                $DS2413Values[] = [
+                    'instanceID' => $InstanceID,
+                    'name'       => IPS_GetName($InstanceID),
+                    'address'    => $Address,
+                    'location'   => IPS_GetLocation($InstanceID)
+                ];
+            }
+
+            // DS2450
+            $DS2450Values = [];
+            foreach ($FoundBattery as $Address => $Device) {
+                $InstanceID = array_search($Address, $InstanceIDListDS2450);
+                if ($InstanceID !== false) {
+                    $AddValue = [
+                        'instanceID' => $InstanceID,
+                        'name'       => IPS_GetName($InstanceID),
+                        'address'    => $Address,
+                        'location'   => IPS_GetLocation($InstanceID)
+                    ];
+                    unset($InstanceIDListDS2450[$InstanceID]);
+                } else {
+                    $AddValue = [
+                        'instanceID' => 0,
+                        'name'       => $this->Translate('Battery') . ' ' . $Device['name'],
+                        'address'    => $Address,
+                        'location'   => ''
+                    ];
+                }
+                $Create = [
+                    'moduleID'      => '{B2963A2B-C5F8-03C3-CC97-6A81A0225EC0}',
+                    'configuration' => ['Address' => $Address]
+                ];
+
+                $AddValue['create'] = array_merge([$Create], $ParentCreate);
+                $DS2450Values[] = $AddValue;
+            }
+            foreach ($InstanceIDListDS2450 as $InstanceID => $Address) {
+                $DS2450Values[] = [
+                    'instanceID' => $InstanceID,
+                    'name'       => IPS_GetName($InstanceID),
+                    'address'    => $Address,
+                    'location'   => IPS_GetLocation($InstanceID)
+                ];
+            }
+
+            $Form['actions'][0]['items'][0]['values'] = $DS18B20Values;
+            $Form['actions'][0]['items'][0]['rowCount'] = count($DS18B20Values) + 1;
+            $Form['actions'][1]['items'][0]['values'] = $DS2413Values;
+            $Form['actions'][1]['items'][0]['rowCount'] = count($DS2413Values) + 1;
+            $Form['actions'][2]['items'][0]['values'] = $DS2450Values;
+            $Form['actions'][2]['items'][0]['rowCount'] = count($DS2450Values) + 1;
+            $this->SendDebug('FORM', json_encode($Form), 0);
+            $this->SendDebug('FORM', json_last_error_msg(), 0);
+            return json_encode($Form);
+        }
+
+        /**
+         * Wird ausgeführt wenn der Kernel hochgefahren wurde.
+         */
+        protected function KernelReady()
+        {
+            $this->ApplyChanges();
+        }
+
+        protected function RegisterParent()
+        {
+            $SplitterId = $this->IORegisterParent();
+            if ($SplitterId > 0) {
+                $IOId = @IPS_GetInstance($SplitterId)['ConnectionID'];
+                if ($IOId > 0) {
+                    $this->SetSummary(IPS_GetProperty($IOId, 'Host'));
+
+                    return;
+                }
+            }
+            $this->SetSummary(('none'));
+        }
+
+        /**
+         * Wird ausgeführt wenn sich der Status vom Parent ändert.
+         */
+        protected function IOChangeState($State)
+        {
+            if ($State == IS_ACTIVE) {
+                // Buffer aller Player laden
+            } else {
+                // Buffer aller Player leeren
+            }
+        }
+
+        /**
+         * IPS-Instanz-Funktion 'OWKONF_GetDeviceInfo'.
+         * Lädt die bekannten Player vom OWSPLIT.
+         *
+         * @result array|bool Assoziiertes Array,  false und Fehlermeldung.
+         */
+        private function GetDeviceInfo()
+        {
+            $count = $this->Send(new OWSPLITData(['server', 'count'], '?'));
+            if (($count === false) || ($count === null)) {
+                return [];
+            }
+            $servers = [];
+            for ($i = 0; $i < $count->Data[0]; $i++) {
+                $serverid = $this->Send(new OWSPLITData(['server', 'id'], [$i, '?']));
+                if ($serverid === false) {
+                    continue;
+                }
+                $id = strtolower(rawurldecode($serverid->Data[1]));
+
+                $serverid = $this->Send(new OWSPLITData(['server', 'ip'], [$i, '?']));
+                if ($serverid === false) {
+                    continue;
+                }
+                $serverid[$id]['ip'] = rawurldecode(explode(':', $serverip->Data[1])[0]);
+                $servername = $this->Send(new OWSPLITData(['server', 'name'], [$i, '?']));
+                if ($servername === false) {
+                    continue;
+                }
+                $players[$id]['name'] = rawurldecode($playername->Data[1]);
+                $servermodel = $this->Send(new OWSPLITData(['server', 'model'], [$i, '?']));
+                if ($servermodel === false) {
+                    continue;
+                }
+                $servers[$id]['model'] = rawurldecode($servermodel->Data[1]);
+            }
+            return $servers;
+        }
+
+        private function GetInstanceList(string $GUID, string $ConfigParam)
+        {
+            $InstanceIDList = array_flip(array_values(array_filter(IPS_GetInstanceListByModuleID($GUID), [$this, 'FilterInstances'])));
+            if ($ConfigParam != '') {
+                array_walk($InstanceIDList, [$this, 'GetConfigParam'], $ConfigParam);
+            }
+            return $InstanceIDList;
+        }
+
+        private function FilterInstances(int $InstanceID)
+        {
+            return IPS_GetInstance($InstanceID)['ConnectionID'] == $this->ParentID;
+        }
+
+        private function GetConfigParam(&$item1, $InstanceID, $ConfigParam)
+        {
+            $item1 = IPS_GetProperty($InstanceID, $ConfigParam);
+        }
+
+        /**
+         * Konvertiert $Data zu einem JSONString und versendet diese an den Splitter.
+         *
+         * @param OWSPLITData $OWSPLITData Zu versendende Daten.
+         *
+         * @return OWSPLITData Objekt mit der Antwort. NULL im Fehlerfall.
+         */
+        private function Send(OWSPLITData $OWSPLITData)
+        {
+            try {
+                $JSONData = $OWSPLITData->ToJSONString('{7A1272A4-CBDB-46EF-BFC6-DCF4A53D2FC7}');
+                $answer = @$this->SendDataToParent($JSONData);
+                if ($answer === false) {
+                    return null;
+                }
+                $result = @unserialize($answer);
+                if ($result === null) {
+                    return null;
+                }
+                $OWSPLITData->Data = $result->Data;
+                return $OWSPLITData;
+            } catch (Exception $exc) {
+                trigger_error($exc->getMessage(), E_USER_NOTICE);
+                return null;
+            }
+        }
 	}
